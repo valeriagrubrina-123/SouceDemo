37f0e8564a43e91089bc072b12fc5b6b6154aafd
valeria.grubrina <valeriagrubrina@gmail.com>
2026-06-03 20:56:52 +0300

    Обновления

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -67,6 +67,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
+        options.addArguments("--headless");
         return options;
     }
 
